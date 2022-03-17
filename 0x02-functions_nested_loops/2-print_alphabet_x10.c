cf50110ca78872e4bcc791ee4f494974c2a2077d
Favour Orji <favouru077@gmail.com>
2022-03-16 18:10:09 -0700

    alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_10 - print 10 times the alphabet, in lower case,
- * followed by a new line
+ * print_alphabet_x10 - print 10 times the alphabet, in lower case, followed by a new line
  */
 void print_alphabet_x10(void)
 {
