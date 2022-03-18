0b52df6bf5f81ee27cacd7d7d681d87d82b2f18b
Favour Orji <favouru077@gmail.com>
2022-03-18 07:23:04 -0700

    Prints diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,24 +6,25 @@
  */
 void print_diagonal(int n)
 {
-	if (n <= 0)
+	int i, j;
+	if (n < 0)
 	{
-		_putchar('\n');
-	} else
+	for (i = 0; i < n; i++)
 	{
-		int i, j;
-		
-		for (i = 0; i < n; i++)
-		{
-			for (j = 0; j < 0; j++)
-			{
-				if (j == i)
-				_putchar('\\');
-				else if (j < i)
-				_putchar(' ');
-			}
-			_putchar('\n');
-		}
+	for (j = 0; j < n; j++)
+	{
+	_putchar(' ');
+	if (j == i)
+	{
+	_putchar('\\');
+	}
+	}
+	_putchar('\n');
+	}
+	}
+	else
+	{
+	_putchar('\n');
 	}
 }
 		
