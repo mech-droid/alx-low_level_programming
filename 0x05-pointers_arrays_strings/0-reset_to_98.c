1375ba7ca0dab78929bbcfe41fb60d7cadb87f33
Favour Orji <favouru077@gmail.com>
2022-03-22 13:49:46 -0700

    using pointer to update the value of a variable

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * reset_to_98 - writes a function that takes a pointer to an int as parameter and updates the value it points to 98
+  * reset_to_98 -  updates the value it points to 98
   * @n: pointer to n
   *
   * Return: void
