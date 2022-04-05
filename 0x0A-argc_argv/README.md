8fdb990cdc2a3f32b7303311b4d93644bfc0b00c
Favour Orji <favouru077@gmail.com>
2022-04-05 02:29:29 -0700

    Arg c and Arg v

---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+File 0-whatsmyname.c is a program that prints its name, followed by a new line.
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,13 @@
+#ifndef FILE_HOLBERTON
+
+#define FILE_HOLBERTON
+
+
+
+int _putchar(char c);
+
+int _atoi(char *s);
+
+
+
+#endif
