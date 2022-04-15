6f784472c2e84aaba5bea46c61db4163e04920ed
Favour Orji <favouru077@gmail.com>
2022-04-15 14:40:09 -0700

    function

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,15 +6,18 @@
 * @array: array of element
 * @size: size of array
 *
+* Return: index
 */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (cmp && array)
 	{
-		return (-1);
+		if (size <= 0)
+			return (-1);
+
 		for (i = 0; i < size; i++)
 		{
 			if ((cmp)(array[i]))
