8a826ece28e6b3682cc2b7f99c61c5522da8a1e6
Favour Orji <favouru077@gmail.com>
2022-04-22 14:11:39 -0700

    linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,15 +11,17 @@
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
-	while (h != 0)
+
+	while (h)
 	{
 		if (h->str == NULL)
 		
-			printf("[%d],(%s)", 0, "nil");
+			printf("[0] (nil)\n");
 		else
-			printf("(%s, %d)", h->str, h->len);
-		count++;
+			printf("[%d], %s\n", h->len, h->str);
+		
 		h = h->next;
+		count++;
 	}
 
 	return(count);
